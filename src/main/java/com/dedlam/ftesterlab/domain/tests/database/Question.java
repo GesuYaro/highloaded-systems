041e6bffa15f95b039d89099a3c1e26ba5ff629a
sshagiev <shagiev.02@gmail.com>
2024-02-07 22:28:24 +0300

    add test entities and migrations

---
+++ src/main/java/com/dedlam/ftesterlab/domain/tests/database/Question.java
@@ -0,0 +1,55 @@
+package com.dedlam.ftesterlab.domain.tests.database;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "questions")
+@Getter
+@Setter
+@ToString
+public class Question {
+
+    public enum QuestionType {ONE_ANSWER, SEVERAL_ANSWERS, OPEN}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "type")
+    private QuestionType questionType;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "question")
+    @ToString.Exclude
+    private List<Answer> answers;
+
+    @ManyToOne
+    @JoinColumn(name = "test_id")
+    private Test test;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Question question = (Question) o;
+        return getId() != null && Objects.equals(getId(), question.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
