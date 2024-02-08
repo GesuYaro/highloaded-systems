70f2a438ec3caad02b7ed4169c948c7a05051247
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2024-02-08 19:38:49 +0300

    Add some default models

---
+++ src/main/java/com/dedlam/ftesterlab/controllers/university/TeachersController.java
@@ -0,0 +1,8 @@
+package com.dedlam.ftesterlab.controllers.university;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/teachers")
+public class TeachersController {
+
+}
