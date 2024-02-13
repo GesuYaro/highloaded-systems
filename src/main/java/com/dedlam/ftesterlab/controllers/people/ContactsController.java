127bdc66625f4bd65d9f0582a5a2ce1e22cc4d83
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2024-02-13 14:13:39 +0300

    Change people controllers by moving parameters to session (user info in jwt-token)

--- src/main/java/com/dedlam/ftesterlab/controllers/people/ContactsController.java
+++ src/main/java/com/dedlam/ftesterlab/controllers/people/ContactsController.java
@@ -1,10 +1,12 @@
 package com.dedlam.ftesterlab.controllers.people;
 
+import com.dedlam.ftesterlab.auth.database.UsersRepository;
 import com.dedlam.ftesterlab.domain.people.database.contacts.Contact;
 import com.dedlam.ftesterlab.domain.people.database.contacts.Contact.ContactType;
 import com.dedlam.ftesterlab.domain.people.database.contacts.PersonContactsInfoRepository;
 import com.dedlam.ftesterlab.domain.people.services.PeopleService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.security.auth.message.AuthException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,28 +17,38 @@ import java.util.UUID;
 public class ContactsController {
   private final PeopleService peopleService;
   private final PersonContactsInfoRepository contactsInfoRepository;
+  private final UsersRepository usersRepository;
 
-  @Autowired
-  public ContactsController(PeopleService peopleService, PersonContactsInfoRepository contactsInfoRepository) {
+  public ContactsController(PeopleService peopleService, PersonContactsInfoRepository contactsInfoRepository, UsersRepository usersRepository) {
     this.peopleService = peopleService;
     this.contactsInfoRepository = contactsInfoRepository;
+    this.usersRepository = usersRepository;
   }
 
-  @GetMapping("/{personId}/contacts")
-  public List<ContactView> contacts(@PathVariable UUID personId) {
+  @GetMapping("/contacts")
+  public List<ContactView> contacts() throws AuthException {
+    var personId = personId();
+
     return contactsInfoRepository.findByPerson_Id(personId).getContacts()
       .stream()
       .map(ContactsController::contactView)
       .toList();
   }
 
-  @PostMapping("/{personId}/contacts")
-  public void addToPerson(@PathVariable UUID personId, @RequestBody List<ContactView> contacts) {
+  @PostMapping("/contacts")
+  public void addToPerson(@RequestBody List<ContactView> contacts) throws AuthException {
+    var personId = personId();
     List<Contact> contactsReq = contacts.stream().map(c -> new Contact(null, c.type, c.value)).toList();
 
     peopleService.bindContacts(personId, contactsReq);
 
     return;
+  }
+
+  private UUID personId() throws AuthException {
+    var username = SecurityContextHolder.getContext().getAuthentication().getName();
+    var userDetails = usersRepository.findUserByUsername(username).orElseThrow(() -> new AuthException("Can't find user"));
+    return peopleService.personByUserId(userDetails.getId()).getId();
   }
 
   private static ContactView contactView(Contact contact) {
