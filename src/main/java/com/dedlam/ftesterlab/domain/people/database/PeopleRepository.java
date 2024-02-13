8484727beeeb3fe34acc4729a0e6eab73c45fad5
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2024-02-13 14:12:02 +0300

    Bind user-information to people

--- src/main/java/com/dedlam/ftesterlab/domain/people/database/PeopleRepository.java
+++ src/main/java/com/dedlam/ftesterlab/domain/people/database/PeopleRepository.java
@@ -3,8 +3,10 @@ package com.dedlam.ftesterlab.domain.people.database;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PeopleRepository extends JpaRepository<Person, UUID> {
+  Optional<Person> findByUserId(UUID userId);
 }
--- src/main/java/com/dedlam/ftesterlab/domain/people/services/PeopleService.java
+++ src/main/java/com/dedlam/ftesterlab/domain/people/services/PeopleService.java
@@ -1,5 +1,6 @@
 package com.dedlam.ftesterlab.domain.people.services;
 
+import com.dedlam.ftesterlab.auth.models.DefaultUser;
 import com.dedlam.ftesterlab.domain.people.database.Person;
 import com.dedlam.ftesterlab.domain.people.database.contacts.Contact;
 import com.dedlam.ftesterlab.domain.people.services.dto.PersonDto;
@@ -8,11 +9,13 @@ import java.util.List;
 import java.util.UUID;
 
 public interface PeopleService {
-  UUID create(PersonDto person);
+  UUID create(DefaultUser user, PersonDto person);
 
   List<Person> people();
 
   Person person(UUID id);
+
+  Person personByUserId(UUID userId);
 
   boolean update(UUID id, PersonDto person);
 
