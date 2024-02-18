5cd714e95fbaae84a1ced48f097587d8f789db1d
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2024-02-19 00:03:32 +0300

    Extract base controller actions

---
+++ src/main/java/com/dedlam/ftesterlab/controllers/BaseController.java
@@ -0,0 +1,31 @@
+package com.dedlam.ftesterlab.controllers;
+
+import com.dedlam.ftesterlab.auth.database.UsersRepository;
+import com.dedlam.ftesterlab.auth.models.DefaultUser;
+import com.dedlam.ftesterlab.domain.people.database.Person;
+import com.dedlam.ftesterlab.domain.people.services.PeopleService;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public abstract class BaseController {
+  private final UsersRepository usersRepository;
+  private final PeopleService peopleService;
+
+  public BaseController(UsersRepository usersRepository, PeopleService peopleService) {
+    this.usersRepository = usersRepository;
+    this.peopleService = peopleService;
+  }
+
+  protected String username() {
+    return SecurityContextHolder.getContext().getAuthentication().getName();
+  }
+
+  protected DefaultUser user() {
+    String username = username();
+    return usersRepository.findUserByUsername(username).orElse(null);
+  }
+
+  protected Person person() {
+    var user = user();
+    return peopleService.personByUserId(user.getId());
+  }
+}
--- src/main/java/com/dedlam/ftesterlab/domain/university/database/StudentsInfoRepository.java
+++ src/main/java/com/dedlam/ftesterlab/domain/university/database/StudentsInfoRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface StudentsInfoRepository extends JpaRepository<StudentInfo, UUID> {
   List<StudentInfo> findAllByIdIn(Set<UUID> ids);
+  Optional<StudentInfo> findStudentInfoByStudent_Id(UUID uuid);
 }
