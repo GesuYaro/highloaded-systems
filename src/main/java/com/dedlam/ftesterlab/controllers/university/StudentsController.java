094f3a007a4124a008b3ac4a170b2c84e7d6a869
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2024-02-19 02:23:32 +0300

    Add validation for getting students info

--- src/main/java/com/dedlam/ftesterlab/controllers/university/StudentsController.java
+++ src/main/java/com/dedlam/ftesterlab/controllers/university/StudentsController.java
@@ -4,15 +4,20 @@ import com.dedlam.ftesterlab.auth.database.UsersRepository;
 import com.dedlam.ftesterlab.controllers.BaseController;
 import com.dedlam.ftesterlab.domain.people.services.PeopleService;
 import com.dedlam.ftesterlab.domain.university.database.StudentsInfoRepository;
-import com.dedlam.ftesterlab.utils.exceptions.MyException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
 @RestController
 @RequestMapping("/students")
 public class StudentsController extends BaseController {
+  private static final Logger logger = LoggerFactory.getLogger(StudentsController.class);
+
   private final StudentsInfoRepository studentsInfoRepository;
 
   public StudentsController(UsersRepository usersRepository, PeopleService peopleService, StudentsInfoRepository studentsInfoRepository) {
@@ -21,9 +26,27 @@ public class StudentsController extends BaseController {
   }
 
   @GetMapping("/info")
-  public ResponseEntity<StudentInfoView> studentInfo() {
+  public ResponseEntity<?> studentInfo() {
     var person = person();
-    var studentInfo = studentsInfoRepository.findStudentInfoByStudent_Id(person.getId()).orElseThrow(() -> new MyException("Can't find student info"));
+    if (person == null) {
+      var user = user();
+      var msg = String.format(
+        "Can't receive student info, because no person-info for user '%s' with id='%s'", user.getUsername(), user.getId()
+      );
+      logger.warn(msg);
+      return new ResponseEntity<>(msg, UNPROCESSABLE_ENTITY);
+    }
+
+    var studentInfoOpt = studentsInfoRepository.findStudentInfoByStudent_Id(person.getId());
+    if (studentInfoOpt.isEmpty()) {
+      var user = user();
+      var msg = String.format(
+        "Can't receive student info, because student-info is not initialized for student '%s'", user.getUsername()
+      );
+      logger.warn(msg);
+      return new ResponseEntity<>(msg, UNPROCESSABLE_ENTITY);
+    }
+    var studentInfo = studentInfoOpt.get();
     var group = studentInfo.getGroup();
     var view = new StudentInfoView(group.getName(), group.getGrade());
     return ResponseEntity.ok(view);
