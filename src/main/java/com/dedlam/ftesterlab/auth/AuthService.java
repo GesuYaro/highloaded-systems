09db0e54f46aaf19d5265caea25c5e91b0ff55a9
Semyon Shagiev <s.shagiev@lipt-soft.ru>
2024-02-28 01:47:40 +0300

    add circuit breaker for auth service

--- src/main/java/com/dedlam/ftesterlab/auth/AuthService.java
+++ src/main/java/com/dedlam/ftesterlab/auth/AuthService.java
@@ -3,7 +3,11 @@ package com.dedlam.ftesterlab.auth;
 import com.dedlam.ftesterlab.auth.dto.CreateUserRequest;
 import com.dedlam.ftesterlab.feign.AuthServiceClient;
 import com.dedlam.ftesterlab.feign.dto.User;
+import com.dedlam.ftesterlab.utils.exceptions.BaseException;
+import com.dedlam.ftesterlab.utils.exceptions.ExceptionType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,32 +15,55 @@ import java.util.Set;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class AuthService {
 
   private final AuthServiceClient authServiceClient;
+  private final CircuitBreaker cb;
+
+  public AuthService(AuthServiceClient authServiceClient, CircuitBreakerFactory circuitBreakerFactory) {
+    this.authServiceClient = authServiceClient;
+    this.cb = circuitBreakerFactory.create("cb-auth");
+  }
 
   public Boolean userExists(UUID id) {
-    return authServiceClient.userExists(id);
+    return cb.run(
+            () -> authServiceClient.userExists(id),
+            throwable -> { throw new BaseException(ExceptionType.BAD_REQUEST); }
+    );
   }
 
   public User user(UUID id) {
-    return authServiceClient.user(id);
+    return cb.run(
+            () -> authServiceClient.user(id),
+            throwable -> { throw new BaseException(ExceptionType.BAD_REQUEST); }
+    );
   }
 
   public Boolean existsByUsername(String username) {
-    return authServiceClient.existsByUsername(username);
+    return cb.run(
+            () -> authServiceClient.existsByUsername(username),
+            throwable -> { throw new BaseException(ExceptionType.BAD_REQUEST); }
+    );
   }
 
   public User findUserByUsername(String username) {
-    return authServiceClient.findUserByUsername(username);
+    return cb.run(
+            () -> authServiceClient.findUserByUsername(username),
+            throwable -> { throw new BaseException(ExceptionType.BAD_REQUEST); }
+    );
   }
 
   public List<User> findUsersByUsernames(Set<String> usernames) {
-    return authServiceClient.findUsersByUsernames(usernames);
+    return cb.run(
+            () -> authServiceClient.findUsersByUsernames(usernames),
+            throwable -> { throw new BaseException(ExceptionType.BAD_REQUEST); }
+    );
   }
 
   public UUID createUser(CreateUserRequest createUserRequest) {
-    return authServiceClient.createUser(createUserRequest);
+    return cb.run(
+            () -> authServiceClient.createUser(createUserRequest),
+            throwable -> { throw new BaseException(ExceptionType.BAD_REQUEST); }
+    );
   }
 }
--- src/main/java/com/dedlam/ftesterlab/controllers/admin/PeopleManagingController.java
+++ src/main/java/com/dedlam/ftesterlab/controllers/admin/PeopleManagingController.java
@@ -3,26 +3,25 @@ package com.dedlam.ftesterlab.controllers.admin;
 import com.dedlam.ftesterlab.auth.AuthService;
 import com.dedlam.ftesterlab.domain.people.models.Person;
 import com.dedlam.ftesterlab.domain.people.services.PeopleService;
+import com.dedlam.ftesterlab.utils.exceptions.BaseException;
+import com.dedlam.ftesterlab.utils.exceptions.ExceptionType;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("admin")
+@RequiredArgsConstructor
 public class PeopleManagingController {
   private final AuthService authService;
   private final PeopleService peopleService;
-
-  public PeopleManagingController(AuthService authService, PeopleService peopleService) {
-    this.authService = authService;
-    this.peopleService = peopleService;
-  }
+  private final CircuitBreakerFactory circuitBreakerFactory;
 
   @GetMapping("people")
   public ResponseEntity<PeopleView> people(@RequestParam int pageNumber) {
@@ -37,8 +36,13 @@ public class PeopleManagingController {
   }
 
   private PersonView personView(Person person) {
+    var cb = circuitBreakerFactory.create("cb");
+    var username = cb.run(
+            () -> authService.user(person.getId()).username(),
+            throwable -> { throw new BaseException(ExceptionType.BAD_REQUEST); }
+    );
     return new PersonView(
-      authService.user(person.getId()).username(), person.getId().toString(), person.getName(),
+      username, person.getId().toString(), person.getName(),
       person.getMiddleName(), person.getLastName(), person.getBirthday()
     );
   }
@@ -57,5 +61,16 @@ public class PeopleManagingController {
     String lastName,
     @JsonFormat(pattern = "dd.MM.yyyy") LocalDate birthday
   ) {
+  }
+
+  @ExceptionHandler(BaseException.class)
+  public ResponseEntity<String> handleBaseException(BaseException e) {
+    HttpStatus status = null;
+    switch (e.getExceptionType()) {
+      case NOT_FOUND -> status = HttpStatus.NOT_FOUND;
+      case FORBIDDEN -> status = HttpStatus.FORBIDDEN;
+      case BAD_REQUEST -> status = HttpStatus.BAD_REQUEST;
+    }
+    return new ResponseEntity<>(e.getMessage(), status);
   }
 }
