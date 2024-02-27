10c53b918f4e1b0fa228c66d96f0c53918deeb36
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2024-02-28 02:36:29 +0300

    Fix after merge

--- src/main/java/com/dedlam/ftesterlab/domain/users/RemoteUserService.java
+++ src/main/java/com/dedlam/ftesterlab/domain/users/RemoteUserService.java
@@ -3,54 +3,75 @@ package com.dedlam.ftesterlab.domain.users;
 import com.dedlam.ftesterlab.auth.AuthServiceClient;
 import com.dedlam.ftesterlab.auth.dto.CreateUserRequest;
 import com.dedlam.ftesterlab.auth.models.User;
+import com.dedlam.ftesterlab.utils.exceptions.BaseException;
+import com.dedlam.ftesterlab.utils.exceptions.ExceptionType;
 import feign.FeignException;
 import jakarta.annotation.Nullable;
-import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @Service
-@RequiredArgsConstructor
 public class RemoteUserService implements UserService {
 
   private final AuthServiceClient authServiceClient;
+  private final CircuitBreaker cb;
+
+  public RemoteUserService(AuthServiceClient authServiceClient, CircuitBreakerFactory cbFactory) {
+    this.authServiceClient = authServiceClient;
+    this.cb = cbFactory.create("cb");
+  }
 
   public Boolean userExists(UUID id) {
     return authServiceClient.userExists(id);
   }
 
   public @Nullable User user(UUID id) {
-    try {
-      return authServiceClient.user(id);
-    } catch (FeignException.NotFound e) {
-      return null;
-    }
+    return doRequestWithCb(() -> {
+      try {
+        return authServiceClient.user(id);
+      } catch (FeignException.NotFound e) {
+        return null;
+      }
+    });
   }
 
   public Boolean existsByUsername(String username) {
-    return authServiceClient.existsByUsername(username);
+    return doRequestWithCb(() -> authServiceClient.existsByUsername(username));
   }
 
   public @Nullable User findUserByUsername(String username) {
-    try {
-      return authServiceClient.findUserByUsername(username);
-    } catch (FeignException.NotFound e) {
-      return null;
-    }
+    return doRequestWithCb(() -> {
+      try {
+        return authServiceClient.findUserByUsername(username);
+      } catch (FeignException.NotFound e) {
+        return null;
+      }
+    });
   }
 
   public List<User> findUsersByUsernames(Set<String> usernames) {
-    return authServiceClient.findUsersByUsernames(usernames);
+    return doRequestWithCb(() -> authServiceClient.findUsersByUsernames(usernames));
   }
 
   public @Nullable UUID createUser(CreateUserRequest createUserRequest) {
-    try {
-      return authServiceClient.createUser(createUserRequest);
-    } catch (FeignException e) {
-      return null;
-    }
+    return doRequestWithCb(() -> {
+      try {
+        return authServiceClient.createUser(createUserRequest);
+      } catch (FeignException e) {
+        return null;
+      }
+    });
+  }
+
+  private <T> T doRequestWithCb(Supplier<T> supplier) {
+    return cb.run(supplier, throwable -> {
+      throw new BaseException(ExceptionType.BAD_REQUEST);
+    });
   }
 }
